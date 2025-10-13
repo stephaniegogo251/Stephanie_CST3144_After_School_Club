42e760e7ffd0c2cff9378f502b9390350b49197e
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2025-10-13 21:20:12 +0400

    added project files

---
+++ lessons.js
@@ -0,0 +1,122 @@
+let lessons = [
+    {
+        id: "ASC001",
+        name: "Music Club",
+        location: "Dubai",
+        price: 150,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC002",
+        name: "Dance Club",
+        location: "Dubai",
+        price: 100,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC003",
+        name: "Writing Club",
+        location: "Dubai",
+        price: 40,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC004",
+        name: "Poetry Club",
+        location: "Dubai",
+        price: 40,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC005",
+        name: "Book Club",
+        location: "Dubai",
+        price: 50,
+        image: "images/basic.png",
+        availableSeats: 5 
+    },
+    {
+        id: "ASC006",
+        name: "Theatre Club",
+        location: "Abu Dhabi",
+        price: 65,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC007",
+        name: "Debate Club",
+        location: "Abu Dhabi",
+        price: 40,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC008",
+        name: "Roller Skating Club",
+        location: "Sharjah",
+        price: 150,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC009",
+        name: "Gaming Club",
+        location: "Sharjah",
+        price: 50,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC010",
+        name: "Drawing Club",
+        location: "Sharjah",
+        price: 70,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC011",
+        name: "Culinary Club",
+        location: "Dubai",
+        price: 40,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC012",
+        name: "Photography Club",
+        location: "Abu Dhabi",
+        price: 70,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC013",
+        name: "Chess Club",
+        location: "Abu Dhabi",
+        price: 50,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC014",
+        name: "Painting Club",
+        location: "Dubai",
+        price: 100,
+        image: "images/basic.png",
+        availableSeats: 5
+    },
+    {
+        id: "ASC015",
+        name: "Swimming Club",
+        location: "Sharjah",
+        price: 100,
+        image: "images/basic.png",
+        availableSeats: 5 
+    }
+]
